ee0bac3ed0122ef59a8f08b5d0c0a86e6a10ab78
Dziadosz Wiktor <57013875+DziadoszWiktor@users.noreply.github.com>
2022-02-01 00:13:26 +0100

    Update settings.py

--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-^(hw9f3!f0dvlv&+rdt7741zis&%!8(i8@ddes-=72h=1e*e&='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # u must to turn it off when u want to pu this online
